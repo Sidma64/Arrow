fe7bbc11bfcb57b435b32f7c6d1aa04f90bdfd7d
Uzay Toprak <56298498+Sidma64@users.noreply.github.com>
2022-01-10 19:23:15 +0300

    Project first version

--- Arrow/ContentView.swift
+++ Arrow/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
+struct Arrow: Shape {
+    var arrowTipSize = 0.2
+    var arrowThickness = 0.3
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: arrowTipSize * rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: arrowTipSize * rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+
+
+
+        return path
+    }
+}
+
 struct ContentView: View {
+    @State private var arrowThickness = 1.0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Arrow()
+            .fill(.red)
+            .frame(width: 50, height: 300)
     }
 }
 
@@ -19,3 +43,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
