00a472f526eeacf9be496794d58aadb9eeb8ee26
Uzay Toprak <56298498+Sidma64@users.noreply.github.com>
2022-01-10 19:21:15 +0300

    Initial Commit

---
+++ Arrow/ArrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrowApp.swift
+//  Arrow
+//
+//  Created by Uzay Toprak on 10.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ArrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
